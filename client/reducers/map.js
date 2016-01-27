ff4b85e8a52d4dcae9c965ee49515798c1c5736f
Irving <Irvingb232@gmail.com>
2016-01-27 15:32:42 -0800

    [Progress] Adds reducer file for map

---
+++ client/reducers/map.js
@@ -0,0 +1,17 @@
+//import { GETMAP } from '../constants'
+//import GOOGLEKEY from ''
+
+const initialState = {
+  center: {lat: 34.0157219, lng: -118.4966245},
+  zoom: 9
+}
+
+/*
+  - make a fetch to google and get data.shops, data.lat, data.lng, and turn fetch = true
+  {
+    lat: '',
+    lng: '',
+    shops: '',
+    fetched: false
+  }
+*/
