e35a5a7833892820fca704029b0efcb58b581153
Irving <Irvingb232@gmail.com>
2016-01-27 15:33:12 -0800

    [Progress] Adds middleware api file

---
+++ client/middleware/api.js
@@ -0,0 +1,6 @@
+// * here we have the actaul call to the endpoints
+
+// import { Schema, arrayOf, normalize } from 'normalizr'
+// import { camelizeKeys } from 'humps'
+// import 'isomorphic-fetch'
+
