c953c00571ae5d14e2804812bf7d852fdcef0a64
Irving <Irvingb232@gmail.com>
2016-01-27 15:32:01 -0800

    [Progress] Adds map actions file

---
+++ client/actions/map.js
@@ -0,0 +1,13 @@
+//import {CALL_API, Schemas} from '../middleware/api'     schemas? CALL_API
+
+// create fetchMap function
+// Relies on the custom API middleware defined in ../middleware/api.js.
+// function fetchUser(login) {
+//   return {
+//     [CALL_API]: {
+//       types: [ USER_REQUEST, USER_SUCCESS, USER_FAILURE ],
+//       endpoint: `users/${login}`,
+//       schema: Schemas.USER
+//     }
+//   }
+// }
