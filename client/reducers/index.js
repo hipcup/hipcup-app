a6aaf585640fe53edc955cc031b9cb9bf40c9631
msarnicki <melindabernrdo@gmail.com>
2016-01-31 16:54:55 -0800

    [Refactor] Removes count.js redux example

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,28 +1,3 @@
-export count from './count'
-export stores from './stores'
-
-// const initialState = {
-//   lat: 59.938043,
-//   lng: 30.337157
-// }
-
-// export function coordinateReducer(state = initialState, action){
-//   switch(action.type){
-//     case UPDATE_COORDINATES:
-//       return Object.assign({}, state, {
-//         lat: action.info.lat,
-//         lng: action.info.lng
-//       })
-//   }
-// }
-
-
-//////////// action creator
-// const UPDATE_COORDINATES = 'UPDATE_COORDINATES';
-
-// const updateCoordinates = (info) => {
-//   return {
-//     type: UPDATE_COORDINATES,
-//     info
-//   }
-// }
+export coffeeRunReducer from './coffeeRun'
+export mapReducer from './map'
+export storeReducer from './stores'
