29962088841d6eb3029bc6f89733a9ddea83ef10
msarnicki <melindabernrdo@gmail.com>
2016-02-05 20:17:08 -0800

    [Server] Creates schema and adds coffee order to coffee run

---
+++ app/controllers/coffeeOrder.js
@@ -0,0 +1,67 @@
+var ObjectId = require('mongoose').Types.ObjectId; 
+var CoffeeOrder = require('../models/coffeeOrder.js');
+var CoffeeRun = require('../models/coffeeRun.js');
+
+exports.placeOrder = function(req, res) {
+
+  var coffeeOrder = new CoffeeOrder ({
+    coffeeRunID:     req.body.coffeeRunID,
+    caffeinatorName: req.body.drinkerName,
+    drinkOrder:      req.body.drinkOrder,
+    modifications:   req.body.modifications
+  });
+
+  console.log('coffeeOrder:', coffeeOrder);
+
+  CoffeeRun.findOne({ "_id": new ObjectId(coffeeOrder.coffeeRunID)}, function (err, coffeeRun) {
+    if (err) return console.error(err);
+    
+    if(!coffeeRun) {
+      console.log("no coffee run found");
+      res.send({
+        err: "Sorry, no coffee run exists at this URL. Try creating a coffee run or double check the url provided.",
+        success: false
+      });
+    } 
+
+    console.log("coffee run exists:", coffeeRun);
+    
+    // if maxNumOrders exceeded 
+    // if(coffeeRun.numOrdersPlaced >= coffeeRun.maxOrders) {
+    //   console.log("num of orders placed is at max orders");
+    //   res.send({
+    //     err: "Sorry, the number of orders for this coffee run has been exceed. Your order cannot be placed.",
+    //     success: false
+    //   });
+    // }
+
+    // if coffee run has expired  
+    // if(coffeeRun.timeUntilRun === 0) {
+        //   console.log("coffee run has expired");
+        //   res.send({
+        //     err: "Sorry, this coffee run has expired. Try creating a new run!",
+        //     success: false
+        //   });
+        // }
+
+    // validate coffeeOrder prior to adding to coffee run
+
+
+    // add coffeeOrder to coffeeRun Orders
+    console.log("coffeeRun.orders", coffeeRun.orders, coffeeOrder);
+    coffeeRun.orders.push(coffeeOrder);
+    coffeeRun.numOrdersPlaced = coffeeRun.numOrdersPlaced++;
+    
+    // save to database
+    coffeeRun.save(function (err) {
+      if (err) {
+        console.log("error saving coffee order in coffee run");
+      }
+      res.json({
+        success: true,
+        message: 'Coffee Order has been placed',
+      });
+    });
+  });
+}
+
--- app/models/coffeeRun.js
+++ app/models/coffeeRun.js
@@ -1,14 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
+var CoffeeOrder = require('./coffeeOrder.js').schema;
 
 var CoffeeRunSchema = new Schema({
-    runnerName:    { type: String, required: true},
-    coffeeShop:    { type: String, required: true},
-    timeStamp:     Date,
-    maxOrders:     Number, 
-    slackChannel:  String,
-    timeUntilRun:  { type: Date, required: true}
+    runnerName:      { type: String, required: true},
+    coffeeShop:      { type: String, required: true},
+    timeStamp:       Date,
+    maxOrders:       Number, 
+    slackChannel:    String,
+    timeUntilRun:    { type: Date, required: true},
+    orders:          [CoffeeOrder],
+    numOrdersPlaced: { type: Number, default: 0}
 });
 
 module.exports = mongoose.model('coffeeRun', CoffeeRunSchema);
