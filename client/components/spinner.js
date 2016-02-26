2619604e9fc9d01b7ea7aba90ddb989f169e24f5
msarnicki <melindabernrdo@gmail.com>
2016-02-25 20:56:16 -0800

    [Fix] misc styling fixes

--- client/components/spinner.js
+++ client/components/spinner.js
@@ -8,7 +8,7 @@ class Spinner extends React.Component {
   render() {
     return (
       <div className='spinner'>
-        <h1>Loading</h1>
+        <h1>Loading...</h1>
         <image src='./../assets/spinner.gif' />
       </div>
     )
