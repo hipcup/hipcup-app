6c98f2d3d890b2f9d322c1b16060cf086401993f
msarnicki <melindabernrdo@gmail.com>
2016-02-25 18:32:05 -0800

    [Refactor] Refactors map component to keep render method pure

--- client/components/searchResultsView/Map.js
+++ client/components/searchResultsView/Map.js
@@ -4,29 +4,36 @@ import GoogleMap from 'google-map-react'
 class Map extends React.Component  {
   constructor(props) {
     super(props);
+    this.displayMap = this.displayMap.bind(this);
+  }
+
+  displayMap() {
+    // if user's lat and lng have been fetched, center map on those coords 
+    if(this.props.userCoords) {
+      return (
+        <GoogleMap 
+          defaultCenter={this.props.center} 
+          center={{lat:this.props.userCoords.lat, lng: this.props.userCoords.lng}}
+          defaultZoom={13}>
+          {this.props.markers}
+        </GoogleMap>
+      )
+    } else {
+      // otherwise render the default map
+      return (
+         <GoogleMap 
+            defaultCenter={this.props.defaultCenter} 
+            defaultZoom={13}>
+            {this.props.markers}
+        </GoogleMap>
+      )
+    }
   }
 
   render() {
-    // render GoogleMap with default center initially 
-    let map = <GoogleMap 
-                defaultCenter={this.props.defaultCenter} 
-                defaultZoom={13}>
-                {this.props.markers}
-              </GoogleMap>
-
-    // if user's lat and lng have been fetched, center map on those coords 
-    if(this.props.userCoords) {
-      map = <GoogleMap 
-                defaultCenter={this.props.center} 
-                center={{lat:this.props.userCoords.lat, lng: this.props.userCoords.lng}}
-                defaultZoom={13}>
-                {this.props.markers}
-            </GoogleMap>
-    }
-
     return (
       <div style={{width:'100%', height:'700px'}}>
-        {map}
+        { this.displayMap() }
       </div>
     )
   }
