7bddec02c18ede9e552de995bbc8c182df807d19
Melinda Bernardo <melindabernrdo@gmail.com>
2016-06-19 17:40:10 -0700

    [Refactor] Updates DB URI

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var google_api_key = require('./server/keys/config.js').google_api_key;
 var port = process.env.PORT || 3468;
 
  // configuration ===============================================================
-mongoose.connect(database.url);
+mongoose.connect(database.DATABASE_URI);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
@@ -23,7 +23,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      
+
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
       res.sendStatus(204);
@@ -47,16 +47,16 @@ var routes = require('./app/routes')(app);
 // database cron jobs  =========================================================
 var expiredRunMethods = require('./app/expiredRunMethods');
 
-// checks for expired coffee runs once every minute 
+// checks for expired coffee runs once every minute
 setInterval(function() {
   var currTime = new Date();
 
   expiredRunMethods.findExpiredRuns(currTime);
   expiredRunMethods.markExpiredRuns(currTime);
-}, 60000); 
+}, 60000);
 
-// delete coffee runs 24 hours after they expire 
-setInterval(expiredRunMethods.deleteExpiredRuns, 86400000); 
+// delete coffee runs 24 hours after they expire
+setInterval(expiredRunMethods.deleteExpiredRuns, 86400000);
 
 // listen ======================================================================
 app.listen(port);
