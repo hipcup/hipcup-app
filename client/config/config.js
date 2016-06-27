fe1a76e7cb87200ea0cbbe474072483b3b1d25a2
Melinda Bernardo <melindabernrdo@gmail.com>
2016-06-26 18:28:33 -0700

    [refactor] updates production url to https

--- client/config/config.js
+++ client/config/config.js
@@ -3,7 +3,7 @@ module.exports = (function(env) {
   if(env === 'local') {
     config.ip = 'http://127.0.0.1:5000';
   } else if (env === 'production') {
-    config.ip = 'http://hipcup-app.herokuapp.com';
+    config.ip = 'https://hipcup-app.herokuapp.com';
   }
   return config;
 }('production'));
